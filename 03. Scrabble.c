47d32f6fd59d55af022a4b577ca4173ecda7c340
Nikhil Muzumdar <65347048+NikhilMuzumdar@users.noreply.github.com>
2022-05-27 09:39:30 +0530

    Add files via upload

---
+++ 03. Scrabble.c
@@ -0,0 +1,50 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    // TODO: Compute and return score for string
+    int sum = 0; // To store the score
+    for (int i = 0, n = strlen(word); i < n; i++) // Iterate over a for to check ord value
+    {
+        int ord = tolower(word[i]); // Lower case the character
+        if ((ord > 96 && ord < 123)) //only if character is a letter, we add it's score, else it's worth 0
+        {
+            sum += POINTS[ord - 97];
+            // printf("The Score is %i\n", sum);
+        }
+    }
+    return sum;
+}
