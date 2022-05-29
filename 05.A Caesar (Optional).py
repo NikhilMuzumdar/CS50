c049cbf75ce767323c171c6191c7805515621bdc
Nikhil Muzumdar <65347048+NikhilMuzumdar@users.noreply.github.com>
2022-05-29 21:42:26 +0530

    Add files via upload

---
+++ 05.A Caesar (Optional).py
@@ -0,0 +1,35 @@
+key = None
+
+print(ord("a"), ord("A"))
+while True:
+    try:
+      key = int(input("Enter key : "))
+      if type(key) == int:
+        break
+      else:
+        continue
+    except ValueError:
+      print("Invalid Input")
+
+text = input("cyphertext  ")
+cipher_text = ""
+key = key % 26
+
+for l in text:
+  asci = ord(l)
+  if (asci > 64 and asci < 91): # For capital letters
+    if asci + key > 90:
+      cipher_text += chr(asci + key - 26)
+    else:
+      cipher_text += chr(asci + key)
+
+  elif (asci > 96 and asci < 123): # For smallcase letters
+    if asci + key > 122:
+      cipher_text += chr(asci + key - 26)
+    else:
+      cipher_text += chr(asci + key)
+
+  else:
+    cipher_text += l
+
+print(cipher_text)
