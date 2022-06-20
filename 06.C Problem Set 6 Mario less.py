b21b0f2f3f794f2d24513bf6c720cce508b228c3
Nikhil Muzumdar <65347048+NikhilMuzumdar@users.noreply.github.com>
2022-06-20 13:24:40 +0530

    Create 06.C Problem Set 6 Mario less.py

---
+++ 06.C Problem Set 6 Mario less.py
@@ -0,0 +1,23 @@
+# Define a function that gets height and handles errors
+def get_height():
+    while True:
+        try:
+            height = int(input(" Enter height in range 1 and 9 (both included): "))
+            if height in range(1, 9):
+                break
+        except:
+            continue
+    return height
+
+
+# Get height from the user
+height = get_height()
+
+
+# Loop through for printing
+for i in range(height):
+    left_space = " " * (height-i-1)
+    hashes = "#" * (i+1)
+    center_space = "  "
+    right = "#" * (i+1)
+    print(f"{left_space}{hashes}{center_space}{right}")
